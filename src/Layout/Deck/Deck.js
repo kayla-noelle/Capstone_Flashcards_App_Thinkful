9c47f910a186e622e78bc0828b6a7e09adb75f47
Kayla <knchavez14@gmail.com>
2021-08-07 20:53:15 -0700

    Updated Deck.js

--- src/Layout/Deck/Deck.js
+++ src/Layout/Deck/Deck.js
@@ -1,74 +1,209 @@
-import React, {useState, useEffect} from "react";
-import {Link, useParams, useRouteMatch} from "react-router-dom";
-import { readDeck, deleteDeck } from "../../utils/api/index";
+import React, { useEffect, useState } from 'react'
+import { Link, useParams, useHistory, useRouteMatch } from 'react-router-dom'
+import { deleteCard, readDeck } from '../../utils/api/index.js'
+import { deleteDeck } from '../../utils/api/index.js'
 
 
-function Deck(){
- const {deckId} = useParams();
- const [deck, setDeck] =useState([]);
- const {url} = useRouteMatch();
- const {id, name, description, cards} = deck;
- 
- useEffect(() => {
-    const abortController = new AbortController()
-    const deckInfo = async () => {
-        const response = await readDeck(deckId, abortController.signal)
-        setDeck(() => response)
-    }
-    deckInfo()
-    return () => abortController.abort()
-}, [deckId]);
+// create a function that retrieves an updated deck's id,
+// fetches it's card data, and sets the deck's useState to 
+// contain it's content
+function Deck() {
+    const [deck, setDeck] = useState([])
+    const {deckId} = useParams()
+    const history = useHistory()
+    const { url } = useRouteMatch()
+    const { id, name, description, cards } = deck
 
-     const deleteHandle= ()=>{
-        if(window.confirm("Are you sure you want to delete this deck? You will not be able to recover it.")){
-            deleteDeck(id);
+    useEffect(() => {
+        const abortController = new AbortController()
+        const deckInfo = async () => {
+            const response = await readDeck(deckId, abortController.signal)
+            setDeck(() => response)
         }
+        deckInfo()
+        return () => abortController.abort()
+    }, [deckId])
+
+    // create a handler for the delete button
+    const deleteHandler = async () => {
+        // if the button is clicked and confirmed by the user, delete the deck using it's id
+        if (window.confirm("Are you sure you want to delete this deck? You will not be able to recover it.")) {
+          await deleteDeck(id)
+          // redirect to the home page
+          history.push('/')
+          // if the delete is not confirmed, leave the deck as is and remain on the same page
+        } else {
+            history.go(0)
+        } 
     }
 
-return (
-    <div>
-        <nav>
-            <ol className="breadcrumb">
-                 {/*link that redirects back to the home page*/}
-                <li className="breadcrumb-item"><Link to={"/"}>Home</Link></li>
-                <li className="breadcrumb-item">Name</li>
-            </ol>
-        </nav>
-        {/* a container holding the card deck, including their name,
+    // if there is no deck or no cards, return the following webpage
+    // that displays "loading..."
+    if (!deck || !cards) {
+        return (
+            <div className="spinner-border text-primary" role="status">
+               <span className="sr-only">
+                   Loading...
+               </span>
+            </div>
+    // if a deck or cards are present, return a webpage with the following content
+    )} else {
+
+        return (
+            <div className="col-9 mx-auto">
+
+                {/* a navigation bar with the following links */}
+                <nav aria-label="breadcrumb">
+                    <ol className="breadcrumb">
+
+                        {/* a link to the home page */}
+                        <li className="breadcrumb-item">
+                            <Link to={"/"}>
+                                <i 
+                                className="fa fa-home" 
+                                aria-hidden="true">
+                                </i> 
+                                Home
+                            </Link>
+                        </li>
+
+                        {/* a string containing the deck's name */}
+                        <li 
+                        className="breadcrumb-item">
+                            {name}
+                        </li>
+                    </ol>
+
+                </nav>
+                
+
+                {/* a container holding the card deck, including their name,
                 description, a button to study it, a button to edit it, a 
                 button to add cards to the deck, and a button to delete it */}
 
-        <div className="card border-0 mb-4">
-            <div className="card-body">
-                <div>
-                    {/*deck name*/}
-                    <h5>{name}</h5>
-                </div>
-                {/*deck description*/}
-                <p>{description}</p>
-                <div className="row px-3">
-                    {/*edit button*/}
-                    <Link to={`/decks/${id}/edit`} className="btn btn-secondary mr-3">Edit</Link>
-                     {/*study button*/}
-                    <Link to={`/decks/${id}/study`} className="btn btn-primary mr-3">Study</Link>
-                     {/*add button*/}
-                     <Link to={`/decks/${id}cards/new`} className="btn btn-primary mr-3">Add Cards</Link>
-                     {/*delete button*/}
-                     <button onClick={deleteHandle} value={id} name="delete" className="btn btn-danger">Delete</button>
-                </div>
-            </div>
-        </div>
-        <div className="row pl-3 pb-2">
-            <h1>Cards</h1>
-        </div>
-        <div className="row card-body"></div>
-        <div className="d-flex justify-content-end p-4">
-        <Link to="/decks/:deckId/cards/:cardId/edit" className="btn btn-secondary">Edit</Link>
-            <button className="btn btn-danger">Delete</button>
-        </div>
-    </div>
-)
+                <div className="card border-0 mb-4">
+                    <div className="card-body">
+                        {/* deck name */}
+                        <div className="row px-3">
+                            <h5 className="card-title">
+                                {name}
+                            </h5>
+                        </div>
 
+                        {/* deck description */}
+                        <p className="card-text">
+                            {description}
+                        </p>
+
+
+                        <div className="row px-3">
+
+                            {/* edit button */}
+                            <Link 
+                                to={`/decks/${id}/edit`} 
+                                className="btn btn-secondary">
+                                    <i className="fa fa-edit" 
+                                    aria-hidden="true">
+                                    </i> 
+                                Edit
+                            </Link>
+
+                            {/* study button */}
+                            <Link 
+                                to={`/decks/${id}/study`} 
+                                className="btn btn-primary ml-3">
+                                    <i className="fa fa-bookmark" 
+                                    aria-hidden="true">
+                                        </i> 
+                                    Study
+                            </Link>
+
+                            {/* add cards button */}
+                            <Link 
+                                to={`/decks/${id}/cards/new`} 
+                                className="btn btn-primary ml-3">
+                                    <i className="fa fa-plus" 
+                                    aria-hidden="true">
+                                    </i> 
+                                    Add Cards
+                            </Link>
+
+                            {/* delete button */}
+                            <button 
+                                onClick={deleteHandler} 
+                                name="delete" value={id} 
+                                className="btn btn-danger ml-auto">
+                                    <i className="fa fa-trash" 
+                                    aria-hidden="true">
+                                    </i>
+                            </button>
+                       
+                        </div>
+
+                    </div>
+                </div>
+
+                {/* a container containing all cards, including their front, back
+                an edit button, and a delete button */}
+
+                <div className="row pl-3 pb-2">
+                    <h1>
+                        Cards
+                    </h1>
+                </div>
+
+                {cards.map((card, index) => 
+                    <div className="row" key={index}>
+                        <div className="col">
+                            <div className="card">
+                                <div className="row card-body">
+
+                                    {/* front */}
+                                    <p className="col-6 card-text">
+                                        {card.front}
+                                    </p>
+
+                                    {/* back */}
+                                    <p className="col-6 card-text">
+                                        {card.back}
+                                    </p>
+                                </div>
+
+                                <div className="d-flex justify-content-end p-4">
+                                    {/* edit button */}
+                                    <Link 
+                                        to={`${url}/cards/${card.id}/edit`} 
+                                        className="btn btn-secondary">
+                                            <i className="fa fa-edit" 
+                                            aria-hidden="true">
+                                            </i> 
+                                        Edit
+                                    </Link>
+
+                                    <button 
+                                        onClick={async () => {
+                                            if (window.confirm("Are you sure you want to delete this card? You will not be able to recover it.")) {
+                                                await deleteCard(card.id)
+                                                history.go(0)
+                                        } else {
+                                            history.go(0)
+                                        } 
+                                    }} 
+                                        name="deleteCard" 
+                                        value={card.id} 
+                                        className="btn btn-danger ml-3">
+                                            <i className="fa fa-trash" 
+                                            aria-hidden="true">
+                                            </i>
+                                    </button>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+        )}
 }
 
-export default Deck;
+export default Deck
